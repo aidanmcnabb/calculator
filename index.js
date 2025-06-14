4536206f2dc0b0a9f0939eba3b64c85e9d40df79
aidanmcnabb <aidan.mcnabb11@gmail.com>
2025-06-13 19:36:51 -0700

    making digital screen of calculator

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ function initiateCalculator() {
     const calculatorContainer = document.createElement('div')
     calculatorContainer.classList.add('calculator-container')
     document.body.appendChild(calculatorContainer)
+
+    const digitalScreen = document.createElement('div')
+    digitalScreen.classList.add('digital-screen')
+    calculatorContainer.appendChild(digitalScreen)
+
+    const calcLogo = document.createElement('span')
+    calcLogo.textContent = 'TOPCALCUS®'
+    calcLogo.classList.add('calc-logo')
+    calculatorContainer.appendChild(calcLogo)
 }
 initiateCalculator()
