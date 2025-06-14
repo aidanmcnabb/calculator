cada5505827573973f5a2a059f5ddb2fe233d619
aidanmcnabb <aidan.mcnabb11@gmail.com>
2025-06-13 21:32:20 -0700

    finished digital screen, next is buttons

--- index.js
+++ index.js
@@ -7,9 +7,27 @@ function initiateCalculator() {
     digitalScreen.classList.add('digital-screen')
     calculatorContainer.appendChild(digitalScreen)
 
+    const digitalScreenInnerShadow = document.createElement('div')
+    digitalScreenInnerShadow.classList.add('inner-shadow')
+    calculatorContainer.appendChild(digitalScreenInnerShadow)
+
     const calcLogo = document.createElement('span')
     calcLogo.textContent = 'TOPCALCUS®'
     calcLogo.classList.add('calc-logo')
     calculatorContainer.appendChild(calcLogo)
+
+    const batteryContainer = document.createElement('div')
+    batteryContainer.classList.add('battery')
+    calculatorContainer.appendChild(batteryContainer)
+
+    for(i = 1; i < 6; i++) {
+        const batteryTick = document.createElement('div')
+        if (i <= 2) {
+            batteryTick.classList.add('grey-tick')
+        } else {
+            batteryTick.classList.add('green-tick')
+        }
+        batteryContainer.appendChild(batteryTick)
+    }
 }
 initiateCalculator()
